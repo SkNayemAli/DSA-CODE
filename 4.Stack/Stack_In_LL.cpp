c8360eeae3587437e131e5d69447ba4a5affc444
Sk Nayem Ali <119183124+SkNayemAli@users.noreply.github.com>
2022-12-12 23:26:40 +0530

    first

---
+++ 4.Stack/Stack_In_LL.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+ int data;
+ Node *next;
+};
+
+
+class Stack
+{
+private:
+ Node *top;
+public:
+ Stack(){top=NULL;}
+ void push(int x);
+ int pop();
+ void Display();
+};
+
+
+void Stack::push(int x)
+{
+ Node *t=new Node;
+ if(t==NULL)
+ cout<<"Stak is Full\n";
+ else
+ {
+ t->data=x;
+ t->next=top;
+ top=t;
+ }
+}
+
+
+int Stack::pop()
+{
+ int x=-1;
+ if(top==NULL)
+ cout<<"Stack is Empty\n";
+ else
+ {
+ x=top->data;
+ Node *t=top;
+ top=top->next;
+ delete t;
+ }
+ return x;
+ 
+}
+
+
+void Stack::Display()
+{
+ Node *p=top;
+ while(p!=NULL)
+ {
+ cout<<p->data<<" ";
+ p=p->next;
+ }
+ cout<<endl;
+}
+
+
+int main()
+{
+ Stack stk;
+
+ stk.push(10);
+ stk.push(20);
+ stk.push(30);
+ 
+ cout<<endl<<"After pushed the stack is: "<<endl;
+ stk.Display();
+
+ cout<<endl<<"Before pop the stack is: "<<endl;
+ stk.Display();
+
+ stk.pop();
+
+ cout<<endl<<"After popped the stack is: "<<endl;
+ stk.Display();
+ cout<<endl;
+ 
+ return 0;
+}
